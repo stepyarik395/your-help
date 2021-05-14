68ef3344c3d8f2e629111fc409354fc46b7bdb38
stepyarik395 <stepyarik395@gmail.com>
2021-05-14 14:02:44 +0300

    add__some_section

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,45 @@ $('.section1__slider').slick({
   fade: true,
   cssEase: 'linear',
 });
+
+$('.section5__slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: true,
+  prevArrow: `<svg class="slick-prev" width="23" height="44" viewBox="0 0 23 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M22 1L1 22L22 43" stroke="#3E8E31" stroke-width="0.8"/>
+</svg>
+`,
+  nextArrow: `<svg class="slick-next width="23" height="44" viewBox="0 0 23 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 1L22 22L1 43" stroke="#3E8E31" stroke-width="0.8"/>
+</svg>
+`,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
